11b9c67fbf042eec8ae8d8c2811a10f7f1cb9f54
Ben <benjamin.plamondon@gmail.com>
2017-11-04 16:38:38 -0400

    test example to find ip of other devices on network

--- client.py
+++ client.py
@@ -1,29 +1,58 @@
-import socket
-import sys
+from socket import *
+network = '192.168.1.'
+def is_up(addr):
+    s = socket(AF_INET, SOCK_STREAM)
+    s.settimeout(0.01)    ## set a timeout of 0.01 sec
+    if not s.connect_ex((addr,135)):    # connect to the remote host on port 135
+        s.close()                       ## (port 135 is always open on Windows machines, AFAIK)
+        return 1
+    else:
+        s.close()
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def run():
+    print  ' '
+    for ip in xrange(1,256):    ## 'ping' addresses 192.168.1.1 to .1.255
+        addr = network + str(ip)
+        if is_up(addr):
+            print '%s \t- %s' %(addr, getfqdn(addr))    ## the function 'getfqdn' returns the remote hostname
+    print    ## just print a blank line
 
-# Connect the socket to the port where the server is listening
-server_address = ('192.168.43.29', 10000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
-try:
+if __name__ == '__main__':
+    print '''I'm scanning the local network for connected Windows machines (and others with samba server running).
+Also, I'll try to resolve the hostnames.
+This might take some time, depending on the number of the PC's found. Please wait...'''
 
-    # Send data
-    message = 'This is the message.  It will be repeated.'
-    print >> sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
+run()
+raw_input('Done')
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(message)
-
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print >> sys.stderr, 'received "%s"' % data
-
-finally:
-    print >> sys.stderr, 'closing socket'
-    sock.close()
+# import socket
+# import sys
+#
+# # Create a TCP/IP socket
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+#
+#
+# # Connect the socket to the port where the server is listening
+# server_address = ('192.168.43.29', 10000)
+# print >>sys.stderr, 'connecting to %s port %s' % server_address
+# sock.connect(server_address)
+# try:
+#
+#     # Send data
+#     message = 'This is the message.  It will be repeated.'
+#     print >> sys.stderr, 'sending "%s"' % message
+#     sock.sendall(message)
+#
+#     # Look for the response
+#     amount_received = 0
+#     amount_expected = len(message)
+#
+#     while amount_received < amount_expected:
+#         data = sock.recv(16)
+#         amount_received += len(data)
+#         print >> sys.stderr, 'received "%s"' % data
+#
+# finally:
+#     print >> sys.stderr, 'closing socket'
+#     sock.close()
--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 import socket
-import sys
+
+# find the local ip address of the server
+hostname = socket.gethostname()
+ip = socket.gethostbyname(hostname)
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+server_address = (ip, 5050)
+print 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
 # Listen for incoming connections
@@ -14,20 +17,20 @@ sock.listen(1)
 
 while True:
     # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
+    print 'waiting for a connection'
     connection, client_address = sock.accept()
     try:
-        print >> sys.stderr, 'connection from', client_address
+        print 'connection from', client_address
 
         # Receive the data in small chunks and retransmit it
         while True:
             data = connection.recv(16)
-            print >> sys.stderr, 'received "%s"' % data
+            print 'received "%s"' % data
             if data:
-                print >> sys.stderr, 'sending data back to the client'
+                print 'sending data back to the client'
                 connection.sendall(data)
             else:
-                print >> sys.stderr, 'no more data from', client_address
+                print 'no more data from', client_address
                 break
 
     finally:
