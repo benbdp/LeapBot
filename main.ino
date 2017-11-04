0d70bc62dfbc7f62138562c4a82e0f11049b888d
benbdp <benjamin.plamondon@gmail.com>
2017-11-04 19:17:08 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -7,8 +7,12 @@ Adafruit_DCMotor *motor_a = AFMS.getMotor(1);
 Adafruit_DCMotor *motor_b = AFMS.getMotor(2);
 Adafruit_DCMotor *motor_c = AFMS.getMotor(3);
 Adafruit_DCMotor *motor_d = AFMS.getMotor(4);
-String inString = "";
-int set_speed;
+String reading = "";
+int comma_index;
+String first_values;
+String second_values;
+int right_values;
+int left_values;
 
 void setup() {
   AFMS.begin(); 
@@ -21,45 +25,60 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-    inString = Serial.readStringUntil('\n'); // read from buffer until newline
-    set_speed = inString.toInt();
+    reading = Serial.readStringUntil('\n'); // read from buffer until newline
+    comma_index = reading.indexOf(',');
+    first_values = reading.substring(0, comma_index);
+    second_values = reading.substring(comma_index+1,reading.length());
+    left_values = first_values.toInt();
+    right_values = second_values.toInt();
+   }
+   right();
+   left();
+}
+
+void right(){
+  //make right turn
+  if (right_values > 0){
+    motor_a->run(FORWARD);
+    motor_b->run(FORWARD);
+    motor_a->setSpeed(right_values);
+    motor_b->setSpeed(right_values);
   }
-  if (set_speed < 0) {
-    reverse();
+  //make left turn
+  if (right_values < 0){
+    motor_a->run(BACKWARD);
+    motor_b->run(BACKWARD);
+    motor_a->setSpeed(abs(right_values));
+    motor_b->setSpeed(abs(right_values));
   }
-  else if (set_speed > 0) {
-    forward();
-  }
-  else if (set_speed == 0) {
-    off();
+  if (right_values == 0){
+    motor_a->run(FORWARD);
+    motor_b->run(FORWARD);
+    motor_a->setSpeed(0);
+    motor_b->setSpeed(0);
   }
 }
 
-
-void reverse(){
-  motor_a->run(FORWARD);
-  motor_b->run(FORWARD);
-  motor_c->run(BACKWARD);
-  motor_d->run(BACKWARD);
-  motor_a->setSpeed(abs(set_speed));
-  motor_b->setSpeed(abs(set_speed));
-  motor_c->setSpeed(abs(set_speed));
-  motor_d->setSpeed(abs(set_speed));
+void left(){
+  //make right turn
+  if (left_values > 0){
+    motor_c->run(FORWARD);
+    motor_d->run(FORWARD);
+    motor_c->setSpeed(left_values);
+    motor_d->setSpeed(left_values);
+  }
+  //make left turn
+  if (left_values < 0){
+    motor_c->run(BACKWARD);
+    motor_d->run(BACKWARD);
+    motor_c->setSpeed(abs(left_values));
+    motor_d->setSpeed(abs(left_values));
+  }
+    if (left_values == 0){
+    motor_c->run(FORWARD);
+    motor_d->run(FORWARD);
+    motor_c->setSpeed(0);
+    motor_d->setSpeed(0);
+  }
 }
-void forward(){
-  motor_a->run(BACKWARD);
-  motor_b->run(BACKWARD);
-  motor_c->run(FORWARD);
-  motor_d->run(FORWARD);
-  motor_a->setSpeed(set_speed);
-  motor_b->setSpeed(set_speed);
-  motor_c->setSpeed(set_speed);
-  motor_d->setSpeed(set_speed);
-}
-void off(){
-  motor_a->setSpeed(set_speed);
-  motor_b->setSpeed(set_speed);
-  motor_c->setSpeed(set_speed);
-  motor_d->setSpeed(set_speed);
-}
-
+  
