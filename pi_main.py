d454cf6328e14ec3de4c4a52edded6c9131b8fb8
Ben <benjamin.plamondon@gmail.com>
2017-11-04 04:54:26 -0400

    trying to read serial

--- pi_main.py
+++ pi_main.py
@@ -1,1 +1,8 @@
-# Author Benjamin Plamondon
+import serial.tools.list_ports
+import serial
+ports = list(serial.tools.list_ports.comports())
+for p in ports:
+    print p[0]
+    ser = serial.Serial(p[0], baudrate=9600, timeout=1)
+    reading = ser.readline()
+    print(reading)
