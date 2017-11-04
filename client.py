a1d66e39a3b70420c9e146ecc42b05f8d2a03bef
Ben <benjamin.plamondon@gmail.com>
2017-11-04 16:44:22 -0400

    going to need to enter the ip of server for now

--- client.py
+++ client.py
@@ -3,8 +3,11 @@ import socket
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+# enter the ip of the server
+ip = raw_input("Enter the IP of server: ")
+
 # Connect the socket to the port where the server is listening
-server_address = ('192.168.43.29', 5050)
+server_address = (ip, 5050)
 print 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 try:
