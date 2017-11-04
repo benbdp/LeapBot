e6092d1f1cc0a786f62579d04fbbb290e1b7840a
Ben <benjamin.plamondon@gmail.com>
2017-11-04 11:22:46 -0400

    test motor control

--- pi_main.py
+++ pi_main.py
@@ -1,8 +1,15 @@
 import serial.tools.list_ports
 import serial
+
 ports = list(serial.tools.list_ports.comports())
 for p in ports:
-    print p[0]
-    ser = serial.Serial(p[0], baudrate=9600, timeout=1)
-    reading = ser.readline()
-    print(reading)
+    print(p[0])
+    ser = serial.Serial(p[0], baudrate=9600, timeout=3)
+
+def write(serial,throttle):
+    serial.write((str(throttle) + '\r\n').encode())
+
+
+if __name__ == "__main__":
+    entry = input("enter: ")
+    write(entry)
