8f7bec65cf1a66bd13abd50e4f0a05907c74eba6
dvo-orlando <dvo.orlando@gmail.com>
2017-11-04 21:05:35 -0400

    Update and rename src/control.py to src.py/leapTest.py

--- src.py/leapTest.py
+++ src.py/leapTest.py
@@ -1,11 +1,4 @@
-################################################################################
-# Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               #
-# Leap Motion proprietary and confidential. Not for distribution.              #
-# Use subject to the terms of the Leap Motion SDK Agreement available at       #
-# https://developer.leapmotion.com/sdk_agreement, or another agreement         #
-# between Leap Motion and you, your company or other organization.             #
-################################################################################
-import socket
+# https://developer.leapmotion.com/
 
 import os, sys, inspect
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
